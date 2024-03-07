a65c31d0a6afb38e52985ef89a2496165ae23181
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-03-07 20:34:45 -0300

    Começando Projeto

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img 
+            src="/imagens/Banner.png" 
+            alt="Banner principal"/>
+        </header>
+    );
+};
+
+export default Banner
