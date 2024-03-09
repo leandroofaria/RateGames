e74bfafd1f198e8307c2603d9f2e82651bd94f94
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-03-08 22:14:00 -0300

    Cadastrando Colaboradores

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './componentes/Banner/index.js';
 import Formulario from './componentes/Formulario/index.js';
+import { useState } from 'react';
 
 function App() {
+
+  const [jogos, setJogos] = useState([])
+
+  const aoNovoJogoAdicionado = (jogo)=> {
+    console.log(jogo)
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoJogoCadastrado = {jogo => aoNovoJogoAdicionado(jogo)} />
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-const Formulario = () => {
+const Formulario = (props) => {
 
   const avaliacoes = 
       ['Ótimo',
@@ -15,7 +15,12 @@ const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault()
-    console.log('Form foi submetido => ', nome, genero, imagem, rank)
+    props.aoJogoCadastrado({
+      nome,
+      genero,
+      imagem,
+      rank
+    })
   }
 
   const [nome, setNome] = useState('')
