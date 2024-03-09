22f5b00cb407147fd13c415c41d3749815e1fdde
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-03-08 21:56:02 -0300

    Controlando o Estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,19 +1,17 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
-    let valor = ''
-
     const aoDigitar = (evento) => {
-        valor = evento.target.value
-        console.log(valor)
+        props.aoAlterar(evento.target.value)
     }
 
     return (
         <div className = "campo-texto">
             <label>{props.label}</label>
             <input
-            value={valor}
+            value={props.valor}
             onChange={aoDigitar}
             placeholder={props.placeholder} 
             required={props.obrigatorio}/>
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
@@ -14,20 +15,34 @@ const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault()
+    console.log('Form foi submetido => ', nome, genero, imagem, rank)
   }
+
+  const [nome, setNome] = useState('')
+  const [genero, setGenero] = useState('')
+  const [imagem, setImagem] = useState('')
+  const [rank, setRank] = useState('')
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do jogo</h2>
         <CampoTexto label="Nome" placeholder="Digite o nome do jogo" 
-        obrigatorio={true}/>
+        obrigatorio={true}
+        valor={nome}
+        aoAlterar={valor => setNome(valor)}/>
         <CampoTexto label="Gênero" placeholder="Digite o gênero do jogo" 
-        obrigatorio={true}/>
+        obrigatorio={true}
+        valor={genero}
+        aoAlterar={valor => setGenero(valor)} />
         <CampoTexto label="Imagem" placeholder="Insira uma imagem do jogo" 
-        obrigatorio={true}/>
+        obrigatorio={true}
+        valor={imagem}
+        aoAlterar={valor => setImagem(valor)} />
         <ListaSuspensa label="Rank" itens={avaliacoes}
-        obrigatorio={true}/>
+        obrigatorio={true}
+        valor={rank}
+        aoAlterar={valor => setRank(valor)}/>
         <Botao>Criar Card</Botao>
       </form>
     </section>
